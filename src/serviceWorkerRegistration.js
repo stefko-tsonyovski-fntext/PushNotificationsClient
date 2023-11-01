eff67ce0a1497962a9d77ab8df07126c85deba2c
stefko.tsonyovski.ext <stefko.tsonyovski.ext@fntsoftware.com>
2023-11-01 16:31:39 +0200

    Check for updates every 10 seconds

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -61,7 +61,7 @@ function registerValidSW(swUrl, config) {
       setInterval(() => {
         registration.update();
         console.debug("Checked for update...");
-      }, 1000 * 60 * 1);
+      }, 1000 * 10 * 1);
 
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;

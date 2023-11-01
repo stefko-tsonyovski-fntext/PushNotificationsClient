031bb5d2f242e480e59eae976be3052475da007f
stefko.tsonyovski.ext <stefko.tsonyovski.ext@fntsoftware.com>
2023-11-01 17:19:15 +0200

    Added logic to handle updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { useEffect } from "react";
 import "./App.css";
+import { useServiceWorker } from "./hooks/useServiceWorker";
 
 function App() {
+  const { waitingWorker, showReload, reloadPage } = useServiceWorker();
+  // decides when to show the toast
+  useEffect(() => {
+    if (showReload && waitingWorker) {
+      console.log("Update is available");
+      // showToast({
+      //   description: (
+      //     <div>
+      //       A new version of this page is available
+      //       <button onClick={() => reloadPage()}>REFRESH</button>
+      //     </div>
+      //   ),
+      // });
+    } //else closeToast();
+  }, [waitingWorker, showReload, reloadPage]);
+
   return (
     <div className="App">
       <button

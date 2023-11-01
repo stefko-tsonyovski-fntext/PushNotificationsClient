25a914b10d431903dacfccd6548d80e2eaeb1526
stefko.tsonyovski.ext <stefko.tsonyovski.ext@fntsoftware.com>
2023-11-01 17:34:54 +0200

    More changes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
 
       {showReload && waitingWorker && (
         <button onClick={reloadPage} id="doIt">
-          Reload
+          Refresh
         </button>
       )}
     </div>
